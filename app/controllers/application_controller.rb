4a664cddc3b4b7d80bc903c984ac88c9eeeb4144
sukesh <sukesh@futurenow.biz>
2011-06-14 14:59:13 +0530

    sample

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+   filter_parameter_logging :password
   
   def authenticate
     if not session[:username] or not session[:password]
